eefdf781f1b3453b35ab7faa2f08b191ebef5a20
gelu@codeindustry.net <gelu@codeindustry.net>
2023-03-28 10:54:30 +0300

    added hangfire authentication

---
+++ Hangfire_SwaggerAuth/Hangfire_SwaggerAuth/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hangfire_SwaggerAuth
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(string connString) : base(GetOptions(connString)) { }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+    }
+}
--- Hangfire_SwaggerAuth/Hangfire_SwaggerAuth/Program.cs
+++ Hangfire_SwaggerAuth/Hangfire_SwaggerAuth/Program.cs
@@ -1,10 +1,16 @@
+using Hangfire;
+using Hangfire_SwaggerAuth;
 using Hangfire_SwaggerAuth.Middlewares;
-using Microsoft.Extensions.DependencyInjection;
+using HangfireBasicAuthenticationFilter;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -35,9 +41,30 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+builder.Services.AddHangfire(config =>
+{
+    config.UseSqlServerStorage(builder.Configuration.GetConnectionString("sqlConnection"));
+});
+
 var app = builder.Build();
 
 app.UseMiddleware<ApiKeyMiddleware>();
+
+// Configure Hangfire
+app.UseHangfireDashboard("/hangfire", new DashboardOptions
+{
+    DashboardTitle = "Hangfire",
+    Authorization = new[]
+    {
+        new HangfireCustomBasicAuthenticationFilter
+        {
+            User = builder.Configuration.GetSection("HangfireCredentials:UserName").Value,
+            Pass = builder.Configuration.GetSection("HangfireCredentials:Password").Value
+        }
+    }
+});
+
+app.UseHangfireServer();
 
 // Configure the HTTP request pipeline.
 
